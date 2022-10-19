f10fca779d651c51a0614e1ad81c390954060ead
Chase White <chase.t.white@hotmail.com>
2022-10-18 23:35:24 -0600

    quiz functionality with question type

--- src/components/TF.js
+++ src/components/TF.js
@@ -1,5 +1,22 @@
-const TF = () => {
-  return <h1>True of False</h1>;
+const TF = ({ checkAnswer }) => {
+  return (
+    <div className='tf-box flex'>
+      <button
+        className='quiz__choice'
+        value={"True"}
+        onClick={(e) => checkAnswer(e.target.value)}
+      >
+        True
+      </button>
+      <button
+        className='quiz__choice'
+        value={"False"}
+        onClick={(e) => checkAnswer(e.target.value)}
+      >
+        False
+      </button>
+    </div>
+  );
 };
 
 export default TF;
